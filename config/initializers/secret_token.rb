a1700147256b72abfa2b5a63f93b082186569460
Bruce Williams <bruwillimail@gmail.com>
2012-12-22 22:16:01 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrgOrg::Application.config.secret_token = '39b4d9a84875b3372f508a75bf1d6dd7af2fb23ac139b12d75842de935b0de7667538365adbb6094c2802d695beebfb3e96586f958b4faffbd0c369541abcaa0'

c4e934e06f06432970813d984e69517983dadb12
Bruce Williams <bruwillimail@gmail.com>
2012-12-23 18:00:22 -0800

    added named routes and contacts page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,19 +4,19 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the h1 'Org Org'" do
-      visit '/static_pages/home'
+    it "should have the h1 'Org-Org'" do
+      visit root_path
       page.should have_selector('h1', :text => 'Org-Org')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title',
                         :text => "Org-Org: Organize Your Organization")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should_not have_selector('title', :text => '| Home')
     end
   end
@@ -24,12 +24,12 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title',
                         :text => "Org-Org: Organize Your Organization | Help")
     end
@@ -38,15 +38,28 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',
                         :text => "Org-Org: Organize Your Organization | About Us")
     end
   end
-  
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit contacts_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contacts_path
+      page.should have_selector('title',
+                    text: "Org-Org: Organize Your Organization | Contact")
+    end
+  end  
 end
